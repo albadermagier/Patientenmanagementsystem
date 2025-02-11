1bf4a10d6ccff9642f96b860b7bb2360dfb86f5f
Michael Kiryokoz <Kiryokoz@hs-pforzheim.de>
2025-02-11 21:49:44 +0100

    Testversion 1(für Klasse Person)

--- Patientenmanagementsystem/Person.h
+++ Patientenmanagementsystem/Person.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <string>
+using namespace std;
 
 class Person {
 private:
@@ -8,8 +10,18 @@ private:
 	string Vname;
 	string Adresse;
 public:
+	Person(int alter, string Name, string Vname, string Adresse) {
+		this->alter = alter;
+		this->Adresse = Adresse;
+		this->Vname = Vname; 
+		this->Name = Name; 
+	}
 	void getName(string Name) { Name = this->Name; }
 	void getVname(string Vmame) { Vname = this->Vname; }
 	void getAdresse(string Adresse) { Adresse = this->Adresse; }
 	void getAlter(string alter) { alter =this->alter; }
+	void ausgabeName() { cout << this->Name << endl; }
+	void ausgabeVname() { cout << this->Vname << endl; }
+	void ausgabeAdresse() { cout << this->Adresse << endl; }
+	void ausgabeAlter() { cout << this->alter << endl; }
 };
---
+++ Patientenmanagementsystem/testmain.cpp
@@ -0,0 +1,11 @@
+#include "Person.h"
+
+int main() {
+
+	Person A(10, "Larry", "Jumbo", "arschleckerstr. 10");
+	A.ausgabeAdresse();
+	A.ausgabeAlter();
+	A.ausgabeName();
+	A.ausgabeVname;
+}
+
