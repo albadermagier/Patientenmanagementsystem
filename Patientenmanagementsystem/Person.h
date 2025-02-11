820c31b745bc62a138fe2d2935e5f8a9ae8061db
Michael Kiryokoz <Kiryokoz@hs-pforzheim.de>
2025-02-11 21:26:37 +0100

    Personenklasse Erweitert

--- Patientenmanagementsystem/Person.h
+++ Patientenmanagementsystem/Person.h
@@ -2,14 +2,14 @@
 #include <iostream>
 
 class Person {
-
 private:
-
-
-
-
-
-
-
-
+	int alter;
+	string Name;
+	string Vname;
+	string Adresse;
+public:
+	void getName(string Name) { Name = this->Name; }
+	void getVname(string Vmame) { Vname = this->Vname; }
+	void getAdresse(string Adresse) { Adresse = this->Adresse; }
+	void getAlter(string alter) { alter =this->alter; }
 };
