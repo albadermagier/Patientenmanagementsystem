d9d68bf34dd2a1422d587c1287898337e842a2ab
albadermagier <eroni502@gmail.com>
2025-02-12 22:33:11 +0100

    Fall attribute und konstruktor erstellt

--- Patientenmanagementsystem/Fall.h
+++ Patientenmanagementsystem/Fall.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Behandlungen.h"
+#include <vector>
+
+class Fall {
+
+private:
+	vector <Behandlungen> behandlungen;
+	int Fallnummer;
+
+public:
+
+	Fall(int Fallnummer) : Fallnummer(Fallnummer) {
+
+	}
+
+};
